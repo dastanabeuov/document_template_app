f3e02450f71782b29d37839bdc4631fdd7f2e8c1
ADK <web.dev.adk@gmail.com>
2024-07-12 15:42:54 +0500

    Add:first commit additional style

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
+  before_action :set_breadcrumbs
+
   def index
+  end
+
+  private
+
+  def set_breadcrumbs
+    @breadcrumbs = [
+      { name: '<i class="bi bi-house"></i>'.html_safe, url: root_path },
+      { name: 'Library', url: root_path },
+      { name: 'Data', current: true }
+    ]
   end
 end
