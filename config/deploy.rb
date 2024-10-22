5914840828c47c9079ebdd49782c8c0e3b688d0f
ADK <web.dev.adk@gmail.com>
2024-10-22 11:27:55 +0500

    Fix:add deploy for Stage branch

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+lock "~> 3.19.1"
+
+set :application, 'document_template_app'
+set :repo_url, 'git@github.com:dastanabeuov/document_template_app.git'
+set :rvm_type, :user
+set :branch, 'stage'
+#set :use_sudo, true
+set :deploy_to, '/home/deploy/document_template_app'
+set :deploy_user, 'deploy'
+set :keep_releases, 3
+
+append :linked_files, 'config/database.yml', 'config/master.key'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'storage'
+
+after 'deploy:publishing', 'passenger:restart'
+
+
+
+set :bundle_cmd, "/home/deploy/.rvm/gems/ruby-3.1.0/bin/bundle"
+set :bundle_dir, "/home/deploy/.rvm/gems/ruby-3.1.0"
+set :bundle_flags, '--deployment'
---
+++ config/deploy/production.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+server '192.168.10.17', user: 'deploy', roles: %w[app web db], primary: true
+
+set :rails_env, :development
+
+set :branch, 'stage'
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+#
+# Global options
+# --------------
+set :ssh_options, {
+  keys: %w[/Users/adk/.ssh/id_rsa],
+  forward_agent: true,
+  auth_methods: %w[publickey password],
+  port: 22
+}
