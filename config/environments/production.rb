22006770e342d7bbaf84568816c502988ea8b928
ADK <web.dev.adk@gmail.com>
2024-10-24 11:48:36 +0500

    Feat:add environment from smtp to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,17 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: '192.168.10.17' } # demo-classifier.herokuapp
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
