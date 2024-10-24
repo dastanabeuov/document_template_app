dd29d8555a04b4ffbc177fa0456ad85419268a2e
ADK <web.dev.adk@gmail.com>
2024-10-24 10:12:58 +0500

    Fix:cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,14 +9,9 @@ set :rvm_type, :user
 set :deploy_to, '/home/deploy/document_template_app'
 set :deploy_user, 'deploy'
 set :keep_releases, 3
+set :rvm_ruby_version, 'ruby-3.3.4'
 
 append :linked_files, 'config/database.yml', 'config/master.key'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'storage'
 
 after 'deploy:publishing', 'passenger:restart'
-
-
-
-set :bundle_cmd, "/home/deploy/.rvm/gems/ruby-3.1.0/bin/bundle"
-set :bundle_dir, "/home/deploy/.rvm/gems/ruby-3.1.0"
-set :bundle_flags, '--deployment'
