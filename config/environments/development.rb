c3f3189d1d5777052e355397d56dde37bd4d312a
ADK <web.dev.adk@gmail.com>
2024-10-31 16:18:38 +0500

    Fix:dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'bimtemplates.kazniisa.kz' }
+  config.action_mailer.default_url_options = { host: 'bimtemplates.kazniisa.kz' }#{ host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
