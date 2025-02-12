ad30290889474ec16257a2bbc664fb81d2bd3eec
ADK <web.dev.adk@gmail.com>
2025-02-12 17:55:37 +0500

    Fix:branch deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@
 server '192.168.10.17', user: 'deploy', roles: %w[app web db], primary: true
 
 set :rails_env, :development
-set :branch, 'staging'
+set :branch, 'datatables_to_resource'
 set :ssh_options, {
   keys: %w[/Users/adk/.ssh/id_rsa],
   forward_agent: true,
