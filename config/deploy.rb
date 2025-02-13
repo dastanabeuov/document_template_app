fc82332dcddd9e47abfd450b29616c69483d013e
ADK <web.dev.adk@gmail.com>
2025-02-13 11:10:02 +0500

    Feat:update ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :rvm_type, :user
 set :deploy_to, '/home/deploy/document_template_app'
 set :deploy_user, 'deploy'
 set :keep_releases, 3
-set :rvm_ruby_version, 'ruby-3.3.4'
+set :rvm_ruby_version, 'ruby-3.2.2'
 
 append :linked_files, '.env', 'config/database.yml', 'config/master.key'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'storage'
