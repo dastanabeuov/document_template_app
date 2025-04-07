3e59d50f3d344ee06b6dc671e08f2b6221759918
ADK <web.dev.adk@gmail.com>
2025-04-07 15:54:47 +0500

    Fix:update registration form and rspec test

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,20 +1,20 @@
 class HomesController < ApplicationController
   def index
-    six_months_ago = Date.today - 6.months
-    @users_by_month = User.where("last_sign_in_at >= ?", six_months_ago)
+    six_months_ago ||= Date.today - 6.months
+    @users_by_month ||= User.where("last_sign_in_at >= ?", six_months_ago)
                           .group_by_month(:last_sign_in_at, last: 6)
                           .count
 
-    @latest_trunsactions_documents = Document.where(
+    @latest_trunsactions_documents ||= Document.where(
       user_id: current_user.id
     ).or(
       Document.where(company_id: current_user.company_ids)
     ).order(created_at: :desc)
     .limit(7)
 
-    @templates = Template.last(4)
+    @templates ||= Template.last(4)
 
-    @breadcrumbs = [
+    @breadcrumbs ||= [
       { name: "<i class='bi bi-house'></i> #{I18n.t('.dashboard')}".html_safe, url: root_path, current: true }
     ]
   end
